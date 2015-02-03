28c9bd9eca6fded73b81b90c3538c8a6d85bf5b1
izerui <liuyuhua69@gmail.com>
2015-02-03 23:13:48 +0800

    init

---
+++ src/main/java/com/izerui/redis/Application.java
@@ -0,0 +1,27 @@
+package com.izerui.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by serv on 2015/1/24.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
