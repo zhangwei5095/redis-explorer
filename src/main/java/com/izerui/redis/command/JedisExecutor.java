39d31165f2427a43cff41465fc6151601a6839a7
liuyuhua69@gmail.com <liuyuhua69@gmail.com>
2015-03-04 11:30:10 +0800

    jedis conn timeout

--- src/main/java/com/izerui/redis/command/JedisExecutor.java
+++ src/main/java/com/izerui/redis/command/JedisExecutor.java
@@ -2,6 +2,7 @@ package com.izerui.redis.command;
 
 import com.izerui.redis.entity.RedisServerConfig;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Protocol;
 
 /**
  * Created by serv on 2015/2/8.
@@ -11,7 +12,7 @@ public class JedisExecutor {
     private Jedis jedis = null;
 
     public JedisExecutor(RedisServerConfig server) {
-        jedis = new Jedis(server.getHost(), server.getPort(), 100);
+        jedis = new Jedis(server.getHost(), server.getPort(), Protocol.DEFAULT_TIMEOUT);
         if (server.getPassword() != null && server.getPassword().length() > 0) {
             jedis.auth(server.getPassword());
         }
